efe603c98bcf694bd73c58495972dc9b9c78afa7
Hack Reactor Students <accounts@hackreactor.com>
2015-11-05 11:37:49 -0800

    Created new mongoose schemas and models for users and links

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,52 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var Schema = require('../config').Schema;
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function(){
+//     this.on('creating', this.hashPassword);
+//   },
+//   // comparePassword: function(attemptedPassword, callback) {
+//   //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//   //     callback(isMatch);
+//   //   });
+//   // },
+//   hashPassword: function(){
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
+
+var users = new Schema({
+  username: {type: String, unique: true},
+  passowrd: String,
+  timestamps: Date
 });
+
+users.pre('save', function(next) {
+  var user = this;
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(user.password, null, null).then(function(hash) {
+      user.password = hash;
+    }));
+})
+
+users.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    if (err) {
+      callback(err);
+    }
+    callback(isMatch);
+  });
+}
+
+
+var User = mongoose.model('User', users);
 
 module.exports = User;
